c4882c01c78d9803d1d93a39d0809e90e4b945fa
David <jdv.bermeo@yavirac.edu.ec>
2022-11-27 23:20:37 -0500

    first commit

---
+++ proyecto1/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PagesRoutingModule } from './pages/pages-routing.module';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { StaticRoutingModule } from './static/static-routing.module';
+import { AuthRoutingModule } from './auth/auth-routing.module';
+import { NotfoundRoutingModule } from './notfound/notfound-routing.module';
+
+
+const routes: Routes = [
+  
+  {path: '', redirectTo:'/dashboard', pathMatch:'full'},//ruta por defecto
+ 
+
+
+];
+//routermodule.forRoot :especifica las rutas principales
+@NgModule({
+  declarations: [],
+  imports:[RouterModule.forRoot(routes),
+  PagesRoutingModule,AuthRoutingModule,NotfoundRoutingModule],
+  
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
